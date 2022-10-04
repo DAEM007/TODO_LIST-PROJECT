44bfa154ef615597adb501f4e96e75cf515996da
EMMANUEL <dammylola299@gmail.com>
2022-10-04 14:51:21 +0100

    Implemented local storage to the Todo of the user's browser

--- todo.js
+++ todo.js
@@ -1,9 +1,12 @@
+//  References to the DOM
  const addForm = document.querySelector('.add');
 
  const list = document.querySelector('.todos');
 
  const search = document.querySelector('.search input');
 
+
+//  Add a Todo
  const generateTemplate = ((todo) => {
 
     const html = `
@@ -12,13 +15,12 @@
             <i class="far fa-trash-alt delete"></i>
         </li>
     `;
-  
+
     list.innerHTML += html;
 
  });
 
  addForm.addEventListener('submit', e => {
-     
     e.preventDefault();
     const todo = addForm.add.value.trim();
 
@@ -27,41 +29,48 @@
         addForm.reset();
     }
 
+    // Setting up the local storage
+    localStorage.setItem('Todo', todo);
+
  });
 
+//  Getting the stored Item from the local storage using the tenary operator
+localStorage.getItem('Todo') ? generateTemplate(localStorage.getItem('Todo')) : '';
 
 
-//  delete todos
-list.addEventListener('click', e =>{
+//  Delete todos
+list.addEventListener('click', e => {
     if(e.target.classList.contains('delete')){
         e.target.parentElement.remove();
     }
 
 });
 
-const filterTodo = ((term) =>{
+// Filter the todo from the search input field
+const filterTodo = ((term) => {
 
     Array.from(list.children)
-        .filter((todo) =>{
+        .filter((todo) => {
             return !todo.textContent.toLowerCase().includes(term);
         })
-        .forEach((todo) =>{
+        .forEach((todo) => {
             todo.classList.add('filtered')
         });
 
     Array.from(list.children)
-        .filter((todo) =>{
+        .filter((todo) => {
             return todo.textContent.toLowerCase().includes(term);
         })
-        .forEach((todo) =>{
+        .forEach((todo) => {
             todo.classList.remove('filtered')
         });
 });
 
-// keyup event
-search.addEventListener('keyup', () =>{
+// Keyup event that triggers the anonymous filter function
+search.addEventListener('keyup', () => {
     const term = search.value.trim().toLowerCase();
     filterTodo(term);
 });
 
 
+// Set up a local storage that would store todos in the user's browser
